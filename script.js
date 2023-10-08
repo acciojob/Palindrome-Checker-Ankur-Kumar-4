f025ddc766a74641b4599189d70efd3e497ced8c
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-10-08 12:15:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+let arr = str.split(" ");
+let word = arr.join("");
 
+ if(word === word.split('').reverse().join('')) {
+   return true;
+ }else{
+   return false;
+ }
 }
 module.exports = palindrome
